50fb65d615a80d717ea3477935e23f2c8b6a1732
Morgan Rhodes <morgan@puppet.com>
2019-06-19 15:15:40 -0700

    what is the host

--- setup/common/011_Install_Puppet_Server.rb
+++ setup/common/011_Install_Puppet_Server.rb
@@ -7,6 +7,6 @@ test_name "Install Puppet Server" do
     :nightly_builds_url => ENV['NIGHTLY_BUILDS_URL'],
     :dev_builds_url => ENV['DEV_BUILDS_URL']
   }
-  puts "master === #{master}, type === #{type(master)}"
+  puts "master === #{master}, type === #{master.class}"
   install_puppetserver_on(master, opts)
 end
